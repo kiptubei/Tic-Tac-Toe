59cc8c1bca4524e7b8dac3ad9ed3663b53b38f09
kiptubei <kiptubei@gmail.com>
2020-06-25 13:41:01 +0300

    Separated main from game logic

---
+++ bin/game.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+class Player
+  @move = []
+  attr_reader :name, :sym
+
+  def initialize(name, sym)
+    @name = name
+    @sym = sym
+  end
+end
+
+class Move
+  @@move = {}
+
+  def initialize(player)
+    @@move.key = player
+  end
+
+  winning_ar = [[1, 2, 3][4, 5, 6][7, 8, 9][1, 4, 7][2, 5, 8][3, 6, 9][1, 5, 9][3, 5, 7]]
+
+  def add_move(name, move)
+    @@move[name] << move if @@move[name].size < 3
+  end
+
+  def check_win
+    if @@move[name].size >= 3
+      if winning_ar.any? { @move[name] }
+        winner = name
+        puts winner
+      end
+    end
+  end
+end
