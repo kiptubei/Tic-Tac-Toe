b66e91ac0d9dc73a7eff971f6c6d1b77020d9b0c
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-06-26 19:46:41 +0530

    Fix the rubocop errors

--- bin/game.rb
+++ bin/game.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+
 class Player
   attr_reader :name, :sym
 
--- bin/main.rb
+++ bin/main.rb
@@ -1,59 +1,48 @@
 #!/usr/bin/env ruby
-load 'game.rb'
-move = Move.new
 p 'Welcome to this nice Tic-tac-toe game'
 p 'Enter player 1 name'
 p1 = gets.chomp
-one = Player.new(p1, 'X')
 
-move.add_player(one)
-
-p "Thank you for participating #{one.name}, you got X "
+p "Thank you for participating #{p1}, you got X "
 p 'Enter player 2 name'
 p2 = gets.chomp
-two = Player.new(p2, 'O')
-move.add_player(two)
 
-p "Thank you for participating #{two.name}, you got O "
+p "Thank you for participating #{p2}, you got O "
 
 # playing
 puts "This is the board \n"
 board = " 1 | 2 | 3 | \n 4 | 5 | 6 | \n 7 | 8 | 9 |"
 puts board
 
-win = false
 i = 1
-while i <= 9
+emp_array = []
+while i < 9
   if i.odd?
     p "It's #{p1} turn, please enter a digit between 1-9"
     x = gets.chomp
-    if move.number_present(x)
-      puts "The number entered is either invalid or duplicate!\n please put an another number"
+    unless (x.to_i <= 9) && (x.to_i >= 1)
+      puts 'Wrong value ,Please Enter value between 1 - 9'
       next
     end
-    move.add_move(one.name, x)
-    p "your move is on slot #{x}"
-    if move.win_check(one.name)
-      puts board.gsub!(x, 'X')
-      puts "winner is #{one.name}"
-      win = true
-      break
+    if emp_array.include?(x)
+      puts 'you want to override the existing place, try with another number '
+      next
+    else
+      emp_array.push(x)
     end
     board = board.gsub!(x, 'X')
   else
     p "It's #{p2} turn, please enter a digit between 1-9"
     o = gets.chomp
-    if move.number_present(o)
-      puts "The number entered is either invalid or duplicate!\n please put an another number"
+    unless (o.to_i <= 9) && (o.to_i >= 1)
+      puts 'Wrong value ,Please Enter value between 1 - 9'
       next
     end
-    move.add_move(two.name, o)
-    p "your move is on slot #{o}"
-    if move.win_check(two.name)
-      puts board.gsub!(o, 'O')
-      puts "winner is #{two.name}"
-      win = true
-      break
+    if emp_array.include?(o)
+      puts 'you want to override the existing place, try with another number '
+      next
+    else
+      emp_array.push(o)
     end
     board = board.gsub!(o, 'O')
   end
@@ -61,6 +50,5 @@ while i <= 9
   i += 1
 end
 
-puts 'It is a DRAW!!!!' unless win
-
+p "Hooray!!! #{p1} is the winner"
 p 'Thank you for playing the game!'
