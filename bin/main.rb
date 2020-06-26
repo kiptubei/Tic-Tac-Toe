42b3fee5a79b5531eb4559f128e02359da03e814
kiptubei <kiptubei@gmail.com>
2020-06-26 11:29:58 +0300

    Display board per move, check for invalid input

--- bin/main.rb
+++ bin/main.rb
@@ -2,27 +2,39 @@
 p 'Welcome to this nice Tic-tac-toe game'
 p 'Enter player 1 name'
 p1 = gets.chomp
+
 p "Thank you for participating #{p1}, you got X "
 p 'Enter player 2 name'
 p2 = gets.chomp
+
 p "Thank you for participating #{p2}, you got O "
 
 # playing
 puts "This is the board \n"
-
-puts " 1 | 2 | 3 | \n 4 | 5 | 6 | \n 7 | 8 | 9 |"
+board = " 1 | 2 | 3 | \n 4 | 5 | 6 | \n 7 | 8 | 9 |"
+puts board
 
 i = 1
-while i <= 9
+
+while i < 9
   if i.odd?
     p "It's #{p1} turn, please enter a digit between 1-9"
     x = gets.chomp
-    p "your move is on slot #{x}"
+    unless (x.to_i <= 9) && (x.to_i >= 1)
+      puts 'Wrong value ,Please Enter value between 1 - 9'
+      x = gets.chomp
+    end
+    board = board.gsub!(x, 'X')
   else
     p "It's #{p2} turn, please enter a digit between 1-9"
     o = gets.chomp
-    p "your move is on slot #{o}"
+    unless (o.to_i <= 9) && (o.to_i >= 1)
+      puts 'Wrong value ,Please Enter value between 1 - 9'
+      o = gets.chomp
+    end
+    board = board.gsub!(o, 'O')
   end
+  puts board
   i += 1
 end
 
